beee0cec51715b9830196c4f890526718d624589
Gabriel Berezin <gabrielberezin123@gmail.com>
2025-02-19 20:46:48 +0000

    Player can now progress to a new room through the maze, each room has consequences.

--- Game.cs
+++ Game.cs
@@ -7,48 +7,70 @@ namespace DungeonExplorer
     internal class Game
     {
         private Player player;
-        private Room currentRoom;
+        private int roomsPassed;
 
         public Game()
         {
             player = new Player("", 0, new List<string>());
-            currentRoom = new Room("");
+            roomsPassed = 0;
         }
         public void Start()
         {
             bool playing = true;
             while (playing)
             {
-                try
+                // Setting up player...
+                Console.WriteLine("Please enter player's name: ");
+                string playerName = Console.ReadLine();
+                player.Name = playerName;
+
+                player.Health = 100;
+
+                player.Inventory = new List<string>();
+
+                Console.WriteLine($"\nPlayer: {player.Name} with {player.Health} health created." +
+                    "\nPress any key to begin.");
+                Console.ReadKey();
+
+                playing = false;
+
+                Console.WriteLine("\nObjective: Exit the maze.");
+                while (player.Health > 0 && roomsPassed < 10)
                 {
-
-                    // Setting up player...
-                    Console.WriteLine("Please enter player's name: ");
-                    string playerName = Console.ReadLine();
-                    player.Name = playerName;
-
-                    player.Health = 100;
-
-                    player.Inventory = new List<string>();
-
-                    Console.WriteLine($"Player: {player.Name} with {player.Health} health created." +
-                        "\nPress any key to begin.");
-                    Console.ReadKey();
-
-                    //playing = false;
-
-                    while (player.Health > 0) 
-                    {
-                        // Actual game logic here.
-                    }
-
+                    PlayTurn();
                 }
-                catch (ArgumentNullException e)
+                if (player.Health <= 0) 
                 {
-                    Console.WriteLine($"Error: {e.ParamName} cannot be null.\nPress any key to continue.");
-                    Console.ReadKey();
+                    Console.WriteLine($"\n\n{player.Name} perished in the maze.");
+                }
+                else
+                {
+                    Console.WriteLine($"\n\n{player.Name} successfully escaped the maze!!");
                 }
             }
         }
+        public void PlayTurn() 
+        {
+            Console.WriteLine("\nChoose a direction: (forward or left or right)");
+            string direction = Console.ReadLine().ToLower();
+
+            Room nextroom;
+
+            switch (direction)
+            {
+                case "forward":
+                case "left":
+                case "right":
+                    nextroom = Room.GetRandomRoom();
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid direction. You stumble around confused.");
+                    return;
+            }
+
+            nextroom.ProcessRoom(player, ref roomsPassed);
+        }
+
     }
 }
--- Room.cs
+++ Room.cs
@@ -1,17 +1,77 @@
-﻿namespace DungeonExplorer
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace DungeonExplorer
 {
+    /// <summary>
+    /// Room class will randomly generate a room with a variety of options.
+    /// 
+    /// Attributes:
+    /// (const)(string) Pass -> A way to generate a new room that is safe
+    /// (const)(string) SlightDamage -> A way to generate a new room that inflicts slight damage
+    /// (const)(string) HeavyDamage -> A way to generate a new room that inflicts heavy damage
+    /// (static)(Random) _random -> A way to generate a random number whenever needed
+    /// 
+    /// Methods:
+    /// GetRandomRoom -> Randomly generates and returns one of three room types
+    /// ProcessRoom -> Proceeds the user with(out) consequences depending on room type
+    /// </summary>
     public class Room
     {
-        private string description;
+        // Room Attributes
+        public const string Pass = "Pass";
+        public const string SlightDamage = "SlightDamage";
+        public const string HeavyDamage = "HeavyDamage";
+        private static Random _random = new Random();
 
-        public Room(string description)
+        // Room Accessors
+        public string Type { get; }
+
+        // Room Constructor
+        public Room(string type)
         {
-            this.description = description;
+            Type = type;
         }
 
-        public string GetDescription()
+        // Room Methods
+        public static Room GetRandomRoom()
         {
-            return description;
+            int randomnum = _random.Next(1, 4);
+            switch (randomnum)
+            {
+                case 1:
+                    return new Room(Pass);
+                case 2:
+                    return new Room(SlightDamage);
+                case 3:
+                    return new Room(HeavyDamage);
+                default:
+                    return new Room(Pass);
+            }
         }
+        public void ProcessRoom(Player player, ref int roomsPassed)
+        {
+            switch (Type)
+            {
+                case Pass:
+                    Console.WriteLine("\nYou found an empty room. You moved ahead!");
+                    roomsPassed++;
+                    break;
+                case SlightDamage:
+                    int damage = _random.Next(5, 16);
+                    player.Health -= damage;
+                    Console.WriteLine($"\nYou fell into a small trap!!\nYou took {damage} damage!");
+                    break;
+                case HeavyDamage:
+                    damage = _random.Next(15, 26);
+                    player.Health -= damage;
+                    Console.WriteLine($"\nA monster attacked you!!\nYou took {damage} damage!");
+                    break;
+                default:
+                    Console.WriteLine("\n\n\nAn error occured genereating your room.");
+                    break;
+            }
+        }
+        
     }
 }
